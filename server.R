421061e5bd47e886ff76490f6dd36abe8c2c363f
Juergen Dietz <jdietz00@googlemail.com>
2017-03-19 18:17:17 +0000

    added documentation and improved argument structure of functions

--- server.R
+++ server.R
@@ -9,7 +9,7 @@ int_to_years <- function(x) paste0(x, ' years')
 
 get_mortgage_data <- function(rate_yearly, duration_years, principal) {
   # Takes in mortgage parameters and outputs a dataframe with useful 
-  # quantities over the whole duration of the mortgage.
+  # quantities over the whole duration of the mortgage at monthly granularity.
   # Columns of output data frame:
   # month: Number of months into the mortgage. The remaining colums show
   #        the state of the mortgage at the end of this month.
@@ -61,17 +61,18 @@ get_tot_balance_buy <- function(mortgage_data, home_price_growth_rate,
 }
 
 
-get_buy_out_running <- function(house_price, duration_years, down_payment, rate_yearly,
-                                 maintenance_rate, inflation_rate, home_insurance_rate) {
-  r <- rate_yearly/12
-  N <- duration_years*12
-  P <- house_price*(1 - down_payment)
+get_buy_out_running <- function(p) {
+  r <- p$rate_yearly/12
+  N <- p$duration_years*12
+  P <- p$house_price*(1 - p$down_payment)
   
   monthly_payment <- r/(1 - (1 + r)^(-N))*P
-  yearly_mortgage_payments <- rep(monthly_payment*12, duration_years)
+  yearly_mortgage_payments <- rep(monthly_payment*12, p$duration_years)
   # The amount for maintenance of the first year grows with inflation
-  maintenance_cost <- house_price*maintenance_rate*(1 + inflation_rate)^(1:duration_years)
-  home_insurance_cost <- house_price*home_insurance_rate*(1 + inflation_rate)^(1:duration_years)
+  maintenance_cost <- p$house_price*p$maintenance_rate*
+    (1 + p$inflation_rate)^(1:p$duration_years - 1)
+  home_insurance_cost <- p$house_price*p$home_insurance_rate*
+    (1 + p$inflation_rate)^(1:p$duration_years - 1)
   
   # the running costs of the buying scenario can be invested in the renting scenario
   buy_out_running <- yearly_mortgage_payments + maintenance_cost + home_insurance_cost
@@ -79,58 +80,50 @@ get_buy_out_running <- function(house_price, duration_years, down_payment, rate_
 }
 
 
-get_balance_rent <- function(rent, buy_out_running, house_price, down_payment, cost_buying,
-                             inv_rate_yearly, rent_growth_rate, duration_years) {
+get_balance_rent <- function(rent, p) {
   # Given a monthly rent this computes a vector with the total balance at the end of each year
-  # in the renting scenario
+  # in the renting scenario.
+  
+  buy_out_running <- get_buy_out_running(p)
   
   # To start out with we have the deposit and the one-off buying costs available
-  balance_rent <- house_price*down_payment + house_price*cost_buying
-  for (i in 1:duration_years) {
+  balance_rent <- p$house_price*p$down_payment + p$house_price*p$cost_buying
+  for (i in 1:p$duration_years) {
     balance_rent <- c(balance_rent, 
-                      tail(balance_rent, 1)*(1 + inv_rate_yearly) + 
-                        buy_out_running[i] - rent*12*(1 + rent_growth_rate)^(i - 1))
+                      tail(balance_rent, 1)*(1 + p$inv_rate_yearly) + 
+                        buy_out_running[i] - rent*12*(1 + p$rent_growth_rate)^(i - 1))
   }
   balance_rent
 }
 
 
-final_rent <- function(rate_yearly, duration_years, house_price, down_payment,
-                       home_price_growth_rate, inflation_rate, live_in_years,
-                       inv_rate_yearly, rent_growth_rate, cost_buying,
-                       cost_selling, maintenance_rate, home_insurance_rate) {
-  buy_out_running <- get_buy_out_running(house_price, duration_years, 
-                                         down_payment, rate_yearly, maintenance_rate,
-                                         inflation_rate, home_insurance_rate)
-  
-  principal <- house_price*(1 - down_payment)
-  mortgage_data <- get_mortgage_data(rate_yearly, duration_years, principal)
-  balance_buy <- get_tot_balance_buy(mortgage_data, 
-                                     home_price_growth_rate, cost_selling, 
-                                     house_price, duration_years)
-  
-  uniroot(function(rent) {
-    get_balance_rent(rent, buy_out_running, house_price, down_payment, cost_buying,
-                     inv_rate_yearly, rent_growth_rate, duration_years)[live_in_years + 1] -
-            balance_buy[live_in_years]
+final_rent <- function(p) {
+  # In: p is a list of parameters from the interface
+  # Out: rent_max is the maximum rent before buying is better
+
+  principal <- p$house_price*(1 - p$down_payment)
+  mortgage_data <- get_mortgage_data(p$rate_yearly, p$duration_years, principal)
+  balance_buy <- get_tot_balance_buy(mortgage_data,
+                  p$home_price_growth_rate, p$cost_selling,
+                  p$house_price, p$duration_years)
+
+  rent_max <- uniroot(function(rent) {
+    get_balance_rent(rent, p)[p$live_in_years + 1] - balance_buy[p$live_in_years]
     }, c(-10000, 100000))$root
+  return(rent_max)
 }
 
 
-get_house_price_plot <- function(rate_yearly, duration_years, house_price, down_payment,
-                                 home_price_growth_rate, inflation_rate, live_in_years,
-                                 inv_rate_yearly, rent_growth_rate, cost_buying,
-                                 cost_selling, maintenance_rate, home_insurance_rate) {
+get_house_price_plot <- function(p) {
   # Plots the change in maximum rent for values around the current house price.
   
-  house_prices <- as.integer(seq(from = floor(house_price*0.8/1000)*1000,
-                      to = floor(house_price*1.2/1000)*1000, length.out = 10))
+  house_prices <- as.integer(seq(from = floor(p$house_price*0.8/1000)*1000,
+                      to = floor(p$house_price*1.2/1000)*1000, length.out = 10))
   
   rents <- sapply(house_prices, FUN = function(hp) {
-    final_rent(rate_yearly, duration_years, house_price = hp, down_payment,
-               home_price_growth_rate, inflation_rate, live_in_years,
-               inv_rate_yearly, rent_growth_rate, cost_buying,
-               cost_selling, maintenance_rate, home_insurance_rate)
+    p_mod <- p
+    p_mod$house_price <- hp
+    final_rent(p_mod)
     })
 
   barplot(rents, names.arg = paste0(as.integer(house_prices/1000), 'k'),
@@ -138,20 +131,16 @@ get_house_price_plot <- function(rate_yearly, duration_years, house_price, down_
           ylim = c(0, max(rents)*1.2))
 }
 
-get_live_in_years_plot <- function(rate_yearly, duration_years, house_price, down_payment,
-                                   home_price_growth_rate, inflation_rate, live_in_years,
-                                   inv_rate_yearly, rent_growth_rate, cost_buying,
-                                   cost_selling, maintenance_rate, home_insurance_rate) {
+get_live_in_years_plot <- function(p) {
   # Plots the change in maximum rent for values around the current number of live-in years.
   
-  live_in_years_vec <- as.integer(seq(from = max(live_in_years - 2, 1),
-                                 to = min(live_in_years + 2, duration_years), by = 1))
+  live_in_years_vec <- as.integer(seq(from = max(p$live_in_years - 2, 1),
+                                 to = min(p$live_in_years + 2, p$duration_years), by = 1))
   
   rents <- sapply(live_in_years_vec, FUN = function(liy) {
-    final_rent(rate_yearly, duration_years, house_price, down_payment,
-               home_price_growth_rate, inflation_rate, live_in_years = liy,
-               inv_rate_yearly, rent_growth_rate, cost_buying,
-               cost_selling, maintenance_rate, home_insurance_rate)
+    p_mod <- p
+    p_mod$live_in_years <- liy
+    final_rent(p_mod)
   })
 
   barplot(rents, names.arg = live_in_years_vec,
@@ -163,7 +152,7 @@ shinyServer(function(input, output, session) {
   
   input_vals <- 
     reactive(list(rate_yearly = perc_to_numeric(input$mortgage_rate),
-              duration_years = input$mortgage_duration,
+              duration_years = years_to_int(input$mortgage_duration),
               house_price = input$house_price,
               down_payment = perc_to_numeric(input$down_payment),
               home_price_growth_rate = perc_to_numeric(input$home_price_growth_rate),
@@ -185,9 +174,11 @@ shinyServer(function(input, output, session) {
                                   value = int_to_years(input$live_in_years_slider))
   )
   
+  
   final_rent_text <- 
+    
     eventReactive(list(input$btn, input$trigger), {
-      max_rent <- do.call(final_rent, input_vals())
+      max_rent <- final_rent(input_vals())
       
       paste0("<font size = 6>",'If you pay less than ',
              '<br>', '<font size = 10><b>', floor(max_rent), '</b><br>', '<font size = 6>',
@@ -196,11 +187,11 @@ shinyServer(function(input, output, session) {
     )
   
   output$house_price_plot = renderPlot({
-    do.call(get_house_price_plot, input_vals())
+    get_house_price_plot(input_vals())
     })
   
   output$live_in_years_plot = renderPlot({
-    do.call(get_live_in_years_plot, input_vals())
+    get_live_in_years_plot(input_vals())
   })
   
   output$maximum_rent = renderText(final_rent_text())
